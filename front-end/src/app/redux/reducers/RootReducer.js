3a2ae5a62e00214b68c4a007326f7e1ca50418a6
AndrewLiu66 <zhaoyu.liu@hotmail.com>
2022-07-17 15:03:12 -0700

    remove file that should not be in github

--- front-end/src/app/redux/reducers/RootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import GraphReducer from './GraphReducer'
-
-const RootReducer = combineReducers({
-    graph: GraphReducer
-})
-
-export default RootReducer
