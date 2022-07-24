6b2e5fc0529f894a90bebc68805baf801d5a0825
AndrewLiu66 <zhaoyu.liu@hotmail.com>
2022-07-24 16:33:11 -0700

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from graph import getInitGraph
 from graph import getUpdatedGraph
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, jsonify, make_response
 from bokeh.embed import json_item
 from flask_cors import CORS, cross_origin
 from flask import send_file
@@ -10,7 +10,8 @@ from flask.helpers import send_from_directory
 from download import generateSpectrogramCsvValue
 from download import generateCsvOctave
 from download import generateCsvSPDF
-
+import ujson as json
+import gzip
 
 app = Flask(__name__, static_folder="build", static_url_path="")
 CORS(app)
@@ -53,22 +54,41 @@ def getUpdate():
 @app.route('/api/downloads', methods=['POST'])
 @cross_origin()
 def download():
-    request_data = request.get_json()
-    startDate = request_data['startDate']
-    endDate = request_data['endDate']
-    location = request_data['location']
-    type = request_data['currType']
-    frequency = request_data['frequency']
-    starttime = pd.Timestamp(startDate)
-    endtime = pd.Timestamp(endDate)
-    slice_data = specs[location].loc[starttime:endtime, :]
+    with app.app_context():
+        request_data = request.get_json()
+        startDate = request_data['startDate']
+        endDate = request_data['endDate']
+        location = request_data['location']
+        type = request_data['currType']
+        frequency = request_data['frequency']
+        starttime = pd.Timestamp(startDate)
+        endtime = pd.Timestamp(endDate)
+        slice_data = specs[location].loc[starttime:endtime, :]
 
-    if type == 'Spectrogram':
-        return generateSpectrogramCsvValue(slice_data)
-    elif type == 'Octave Band':
-        return generateCsvOctave(slice_data, location, frequency)
-    else:
-        return generateCsvSPDF(slice_data)
+        if type == 'Spectrogram':
+            return generateSpectrogramCsvValue(slice_data, location)
+        elif type == 'Octave Band':
+            return generateCsvOctave(slice_data, location, frequency)
+        else:
+            return generateCsvSPDF(slice_data)
+
+        # content = gzip.compress(json.dumps(data).encode('utf8'), 5)
+        # response = make_response(content)
+        # response.headers['Content-length'] = len(content)
+        # response.headers['Content-Encoding'] = 'gzip'
+        # return response
+
+
+@app.route('/api/test', methods=['GET'])
+@cross_origin()
+def download2():
+    slice_data = specs['axial_base'].loc['2018-03-12 00:00:05':'2018-03-13 02:03:24', :]
+    slice_frame = slice_data.to_dataframe()
+    slice_frame = slice_frame.reset_index()
+    slice_frame['axial_base'] = slice_frame['axial_base'].fillna(0)
+    slice_frame['time'] = slice_frame['time'].astype(str)
+    slice_dict = slice_frame.to_dict('records')
+    return jsonify({"data": slice_dict})
 
 
 @app.route('/')
--- download.py
+++ download.py
@@ -4,20 +4,26 @@ from graph import get_spdf
 import pandas as pd
 import numpy as np
 # helper method
+
+
 def median(df, location):
     return df[location].median()
+
 
 def firstQuartile(df, location):
     return int(df[location].quantile([0.25]))
 
+
 def thirdQuartile(df, location):
     return int(df[location].quantile([0.75]))
+
 
 def getLowerBound(df, location):
     Q1 = df[location].quantile(0.25)
     Q3 = df[location].quantile(0.75)
     IQR = Q3 - Q1
     return Q1 - (1.5 * IQR)
+
 
 def getUpperBound(df, location):
     Q1 = df[location].quantile(0.25)
@@ -26,11 +32,16 @@ def getUpperBound(df, location):
     return Q3 + (1.5 * IQR)
 
 # generate method
-def generateSpectrogramCsvValue(slice_data):
+
+
+def generateSpectrogramCsvValue(slice_data, location):
     slice_frame = slice_data.to_dataframe()
     slice_frame = slice_frame.reset_index()
+    # slice_frame['time'] = slice_frame['time'].astype(str)
+    slice_frame[location] = slice_frame[location].fillna(0)
     slice_dict = slice_frame.to_dict('records')
-    return jsonify({'data': slice_dict})
+    return jsonify({"data": slice_dict})
+
 
 def generateCsvSPDF(spec_slice):
     spdf = get_spdf(spec_slice, fs_hz=200)
@@ -42,20 +53,22 @@ def generateCsvSPDF(spec_slice):
     L90 = spdf['90']
     L95 = spdf['95']
     L99 = spdf['99']
-    spdf_df = pd.DataFrame(zip(freq, L1, L5, L10, L50, L90, L95, L99), columns=['Frequency','Level 1', 'Level 5', 'Level 10', 'Level 50', 'Level 90', 'Level 95', 'Level 99'])
+    spdf_df = pd.DataFrame(zip(freq, L1, L5, L10, L50, L90, L95, L99), columns=[
+                           'Frequency', 'Level 1', 'Level 5', 'Level 10', 'Level 50', 'Level 90', 'Level 95', 'Level 99'])
     spdf_dict = spdf_df.to_dict('records')
-    return jsonify({'data': spdf_dict})
+    return jsonify({"data": spdf_dict})
+
 
 def generateCsvOctave(slice_data, location, f0=50):
     df_octave = generateDfForOctaveBox(f0, slice_data, location)
-    temp = df_octave.groupby('date').apply(lambda x:pd.Series({
-                                        'median':median(x, location),
-                                        'firstQuartile':firstQuartile(x, location),
-                                        'thirdQuartile':thirdQuartile(x, location),
-                                        'min': getLowerBound(x, location),
-                                        'max': getUpperBound(x, location)
-                                        }))
+    temp = df_octave.groupby('date').apply(lambda x: pd.Series({
+        'median': median(x, location),
+        'firstQuartile': firstQuartile(x, location),
+        'thirdQuartile': thirdQuartile(x, location),
+        'min': getLowerBound(x, location),
+        'max': getUpperBound(x, location)
+    }))
 
     temp = temp.reset_index()
     temp = temp.to_dict('records')
-    return jsonify({'data': temp})
+    return jsonify({"data": temp})
